9a1b5f7de25c3723e6fdcb74c28fd8b7085d6b4c
Florian Joerg <florian.joerg@im-c.at>
2024-11-04 16:58:01 +0100

    update css

--- components/tutorial/tutorialcard.tsx
+++ components/tutorial/tutorialcard.tsx
@@ -8,12 +8,12 @@ interface TutorialCardProps {
 const TutorialCard = ({ tutorial }: TutorialCardProps) => {
   return (
     <div
-      className="tutorialcard group relative flex aspect-square background-contain bg-no-repeat bg-center rounded-2xl cursor-pointer shadow-lg overflow-hidden"
+      className="tutorialcard group relative flex aspect-square bg-cover bg-no-repeat bg-center rounded-2xl cursor-pointer shadow-lg overflow-hidden"
       key={tutorial.id}
       onClick={() => window.open(tutorial.link, "_blank")}
       style={{ backgroundImage: `url(${tutorial.imagelink})` }}
     >
-      <div className="tutorialcardtitle bg-white text-foreground absolute bottom-0 left-0 right-0 h-14 flex justify-center items-center ease-in-out duration-300 group-hover:h-24 group-hover:rounded-md">
+      <div className="z-10 tutorialcardtitle bg-white text-foreground absolute bottom-0 left-0 right-0 h-14 flex justify-center items-center ease-in-out duration-300 group-hover:h-24 group-hover:rounded-md">
         <span className="tutorialcarttext px-1 text-center">
           {tutorial.title}
         </span>

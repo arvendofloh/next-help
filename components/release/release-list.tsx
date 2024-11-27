a888c1e6a8e7b90476d573dec19813139718521a
Florian Jörg <florian.joerg@im-c.at>
2024-11-27 16:46:56 +0100

    fix release list padding

--- components/release/release-list.tsx
+++ components/release/release-list.tsx
@@ -8,7 +8,7 @@ interface ReleaseNoteProps {
 
 const ReleaseList = ({ releaseNotes, locale }: ReleaseNoteProps) => {
   return (
-    <main className="h-auto">
+    <main className="px-8 h-auto w-full max-w-7xl mx-auto">
       {releaseNotes.map((release: ReleaseNote, i: number) => (
         <Release release={release as ReleaseNote} key={i} locale={locale} />
       ))}
